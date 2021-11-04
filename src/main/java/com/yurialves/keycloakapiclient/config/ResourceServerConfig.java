c744ae5fe676f37651dfa2f23b19c58774feda67
Yuri Alves <yurileader@gmail.com>
2021-11-03 23:47:07 -0300

    Adicionando configurações de uri Keycloak

--- src/main/java/com/yurialves/keycloakapiclient/config/ResourceServerConfig.java
+++ src/main/java/com/yurialves/keycloakapiclient/config/ResourceServerConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,20 +17,24 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/clientes").permitAll()
-                .anyRequest().permitAll()
-                .and()
+                .antMatchers("/clientes").hasAuthority("ROLE_GERENTE")
+                .anyRequest().authenticated()
+              .and()
                 .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.authorizeRequests()
-//                .antMatchers("/clientes").hasAuthority("ROLE_GERENTE")
-//                .anyRequest().authenticated()
-//              .and()
-//                .csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//              .and()
-//                .oauth2ResourceServer()
-//                .jwt()
-//                .jwtAuthenticationConverter();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+              .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(getJwtAuthenticationConverter());
+    }
+
+    JwtAuthenticationConverter getJwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("roles");
+        converter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return authenticationConverter;
     }
 }
