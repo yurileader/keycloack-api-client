825f25ba409444248aa054ff0b132934eba98fb4
Yuri Alves <yurileader@gmail.com>
2021-11-03 21:21:07 -0300

    Configuração inicial

---
+++ src/main/java/com/yurialves/keycloakapiclient/KeycloakApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.yurialves.keycloakapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakApiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakApiClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yurialves/keycloakapiclient/KeycloakApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurialves.keycloakapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakApiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
