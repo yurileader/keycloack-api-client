fcfc9de1335f0ec582dbfa672b87d8dbcce6dbf7
Yuri Alves <yurileader@gmail.com>
2021-11-03 23:48:10 -0300

    Adicionando autorização no endpoint clientes, apenas para ROLE_GERENTE

--- src/main/java/com/yurialves/keycloakapiclient/controller/ClientesController.java
+++ src/main/java/com/yurialves/keycloakapiclient/controller/ClientesController.java
@@ -3,6 +3,7 @@ package com.yurialves.keycloakapiclient.controller;
 import com.yurialves.keycloakapiclient.model.Clientes;
 import com.yurialves.keycloakapiclient.service.ClientesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class ClientesController {
 
     private final ClientesService clientesService;
 
+    @PreAuthorize("hasAuthority('ROLE_GERENTE')")
     @GetMapping
     public List<Clientes> buscarListaClientes (){
         return  clientesService.buscarClientes();
